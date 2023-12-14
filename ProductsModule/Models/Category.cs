ba58a0609c677427816bada82e661bc99a892a07
MarRac <marekraczkowski5@gmail.com>
2023-12-14 19:59:45 +0100

    updated Category model

--- ProductsModule/Models/Category.cs
+++ ProductsModule/Models/Category.cs
@@ -1,6 +1,15 @@
-﻿namespace ProductsModule.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductsModule.Models
 {
 	public class Category
 	{
+
+		[Key]
+		public int CategoryId { get; set; }
+
+		public string Name { get; set; }
+
+		public bool IsDeleted { get; set; }
 	}
 }
