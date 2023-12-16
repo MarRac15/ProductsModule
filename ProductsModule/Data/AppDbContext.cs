00a53919b12510037250caf65fc67404defab19b
MarRac <marekraczkowski5@gmail.com>
2023-12-16 23:26:22 +0100

    added Product class and updated AppDbContext

--- ProductsModule/Data/AppDbContext.cs
+++ ProductsModule/Data/AppDbContext.cs
@@ -11,14 +11,19 @@ namespace ProductsModule.Data
 		}
 
 		public DbSet<Category> Tb_Categories { get; set; }
+		public DbSet<Product> Tb_Products { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
 				new Category { CategoryId = 1, Name = "Food", IsDeleted = false },
                 new Category { CategoryId = 2, Name = "Toys", IsDeleted = false },
                 new Category { CategoryId = 3, Name = "Tech", IsDeleted = false }
                 );
-        }
+
+			modelBuilder.Entity<Product>().Property(e => e.CreationDate).HasColumnType("date");
+			
+
+		}
     }
 }
---
+++ ProductsModule/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductsModule.Models
+{
+	public class Product
+	{
+        public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public bool IsDeleted { get; set; }
+
+		[Column(TypeName ="Date")]
+		public DateTime CreationDate { get; set; }
+
+		[MaxLength(300)]
+		public string ImageUrl { get; set; }
+    }
+}
